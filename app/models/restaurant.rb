1f0518de9a36235b5f4cf2f1f162812c382bcbec
Patricia Martinez <patri.martinez.pena@gmail.com>
2019-03-13 11:42:05 +0100

    add migrations and validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,5 @@
 class Restaurant < ApplicationRecord
+  has_many :review, dependent: :destroy
+  validates :name, :address, :category, presence: true
+  validates :stars, inclusion: { in: ['chinese', 'italian', 'japanese', 'french', 'belgian'] }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  validates :restaurant, :content, :rating, presence: true
+  validates :validates, inclusion: { in: ['0', '1', '2', '3', '4', '5'] }
 end
